826fac2073aabbc7ff0622096918092c7a0fde4e
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-05 19:48:41 -0800

    adjust js code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 // //hint: Math.floor(Math.random() * 100)
 
 const container = document.getElementById("numberContainer");
-
 function primeNum(b) {
   if (b < 2) return false
   for (var i = 2; i <= Math.sqrt(b); i++) {
@@ -15,13 +14,13 @@ function primeNum(b) {
 }
     
 for (let i = 1; i <= 100; i++) {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const randomNumber = Math.floor(Math.random() * 100) + 1
   console.log(randomNumber)
 
-  const numberElement = document.createElement("div");
-  numberElement.className = "numberElement";
-  numberElement.textContent = randomNumber;
-  container.appendChild(numberElement);
+  const numberElement = document.createElement("div")
+  numberElement.className = "numberElement"
+  numberElement.textContent = randomNumber
+  container.appendChild(numberElement)
 
 if (i % 2 === 0) {
   numberElement.classList.add('even')
@@ -33,33 +32,3 @@ if (primeNum(i)) {
 }
   container.appendChild(numberElement)
 }
-
-// const container = document.getElementById("numberContainer");
-
-// function primeNum(b) {
-//   if (b < 2) return false;
-//   for (var i = 2; i <= Math.sqrt(b); i++) {
-//     if (b % i === 0) return false;
-//   }
-//   return true;
-// }
-
-// for (let i = 1; i <= 100; i++) {
-//   const randomNumber = Math.floor(Math.random() * 100) + 1;
-//   console.log(randomNumber);
-
-//   const numberElement = document.createElement("div");
-//   numberElement.className = "numberElement";
-//   numberElement.textContent = randomNumber;
-//   container.appendChild(numberElement);
-
-//   if (randomNumber % 2 === 0) {
-//     numberElement.classList.add("even");
-//   } else {
-//     numberElement.classList.add("odd");
-//   }
-
-//   if (primeNum(randomNumber)) {
-//     numberElement.classList.add("prime");
-//   }
-// }
