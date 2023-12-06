d746cc671bccf8544aa9834aa5af958e6125cf5a
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-06 03:45:09 -0800

    adjust linter errors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // //prime numbers background is red
 // //hint: Math.floor(Math.random() * 100)
 
-const container = document.getElementById("numberContainer");
+const container = document.getElementById('numberContainer');
 function primeNum(b) {
   if (b < 2) return false
   for (var i = 2; i <= Math.sqrt(b); i++) {
@@ -12,23 +12,22 @@ function primeNum(b) {
   }
   return true
 }
-    
+
 for (let i = 1; i <= 100; i++) {
   const randomNumber = Math.floor(Math.random() * 100) + 1
   console.log(randomNumber)
 
-  const numberElement = document.createElement("div")
-  numberElement.className = "numberElement"
+  const numberElement = document.createElement('div')
+  numberElement.className = 'numberElement'
   numberElement.textContent = randomNumber
   container.appendChild(numberElement)
 
-if (i % 2 === 0) {
-  numberElement.classList.add('even')
-} else {
-  numberElement.classList.add('odd')
-} 
-if (primeNum(i)) {
-  numberElement.classList.add("prime")
-}
-  container.appendChild(numberElement)
+  if (i % 2 === 0) {
+    numberElement.classList.add('even')
+  } else {
+    numberElement.classList.add('odd')
+  }
+  if (primeNum(i)) {
+    numberElement.classList.add('prime')
+  }
 }
