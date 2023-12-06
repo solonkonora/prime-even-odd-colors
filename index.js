a70978220792f8ae70e4c07f7a3dca79e3f4306a
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-05 18:37:24 -0800

    add js file

---
+++ index.js
@@ -0,0 +1,32 @@
+//create a div container in HTML and create 100 to 100 numbers dynamically and append
+//even numbers background is green
+//odd numbers background is yellow
+//prime numbers background is red
+//hint: Math.floor(Math.random() * 100)
+
+const container = document.getElementById("numberContainer");
+
+function primeNum(b) {
+  if (b < 2) return false
+  for (var i = 2; i <= Math.sqrt(b); i++) {
+    if (b % i === 0) return false
+  }
+  return true
+}
+    
+for (let i = 1; i <= 100; i++) {
+  const numberElement = document.createElement("div");
+  numberElement.className = "numberElement";
+  numberElement.textContent = i;
+  container.appendChild(numberElement);
+
+if (i % 2 === 0) {
+  numberElement.classList.add('even')
+} else {
+  numberElement.classList.add('odd')
+} 
+if (primeNum(i)) {
+  numberElement.classList.add("prime")
+}
+  container.appendChild(numberElement)
+}
